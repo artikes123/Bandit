32da58c007da19b93dc00b07822745f0a9e42702
Artun <artun.erol@yahoo.com>
2021-06-08 20:59:46 +0300

    Setting Up Project

---
+++ Bandit - IOS Academy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bandit - IOS Academy
+//
+//  Created by Artun Erol on 8.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
